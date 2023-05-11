733a89331c5acc980cacdb0db17bb08c0256d32e
Gabriel De Marco <lino.dmarco@gmail.com>
2023-05-11 12:28:17 -0300

    refactor: fix bundle size on mui lib

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,10 +1,16 @@
-import {Container, Typography, Card, Grid, Box, Link} from "@mui/material";
-import BackgroundImage from "@assets/background.jpg";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
 import {Link as RouterLink} from "react-router-dom";
 import RecyclingIcon from "@mui/icons-material/Recycling";
 import * as colors from "@mui/material/colors";
 import styled from "@emotion/styled";
 import {Header} from "@components/Header";
+import BackgroundImage from "@assets/background.jpg";
+import {createSxStyles} from "@helpers/createSxStyles";
 
 const PRODUCTS = [
   {
@@ -51,44 +57,49 @@ export default function HomeView() {
         <Typography>&quot;Faça a diferença no planeta! Pratique a coleta seletiva de lixo em sua casa, escola ou trabalho. Separando o lixo reciclável do orgânico, você está ajudando a preservar o meio ambiente e a garantir um futuro sustentável para as próximas gerações. Vamos juntos cuidar do nosso planeta! Comece agora mesmo a separar seu lixo e contribua para um mundo melhor.&quot;</Typography>
         <Subtitle variant='h5'>Quais os tipos de materias que podem ser reciclados</Subtitle>
         <Grid container rowSpacing={{xs: 1, md: 2}} columnSpacing={{xs: 1, md: 2}}
-          sx={{
-          px: {xs: 0, md: 2}
-          }}
+          sx={productsStyles}
         >
           {PRODUCTS.map((product, i) => (
             <Grid key={String(i + 1)} item md={4}
               xs={6}
             >
-              <Card sx={{
-                  p: 1,
-                  aspectRatio: "1 / 1",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "space-around"
-              }}
-              >
-                <RecyclingIcon sx={{fontSize: 120, color: product.color}} />
+              <Card sx={productCardStyles}>
+                <RecyclingIcon sx={{...productIconStyles, color: product.color}} />
                 <Typography paragraph variant='h5' sx={{textAlign: "center"}}>{product.label.toUpperCase()}</Typography>
               </Card>
             </Grid>
           ))}
         </Grid>
-        <Box sx={{
-            mt: 2,
-            p: {xs: 2, md: 10},
-            display: "flex",
-            flexDirection: {xs: "column", md: "row"},
-            alignItems: "center",
-            justifyContent: "space-between",
-        }}
-        >
+        <Box sx={footerStyles}>
           {LINKS.map(link => <Link key={link} component={RouterLink} to="/sobre">{link}</Link>)}
         </Box>
       </Container>
     </>
   );
 }
+
+const productsStyles = createSxStyles({
+  px: {xs: 0, md: 2}
+});
+
+const productCardStyles = createSxStyles({
+  p: 1,
+  aspectRatio: "1 / 1",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "space-around"
+});
+
+const productIconStyles = createSxStyles({fontSize: 120,});
+const footerStyles = createSxStyles({
+  mt: 2,
+  p: {xs: 2, md: 10},
+  display: "flex",
+  flexDirection: {xs: "column", md: "row"},
+  alignItems: "center",
+  justifyContent: "space-between",
+});
 
 const ImageBackground = styled.img`
   width: 100%;
